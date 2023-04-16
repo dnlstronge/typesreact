3e58e4a6be8d674fa4d49cd84622db5bdcfc3573
Dan Stronge <dnlstronge@gmail.com>
2023-04-16 11:46:15 +0100

    [x] - format

--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import classes from "./Todos.module.css"
-
+import classes from "./Todos.module.css";
 
 import { PropsWithChildren } from "react";
- 
-const Todos: React.FC<PropsWithChildren<{items: string[]}>> = ({ children, items }) => {
-    return (
-        <ul>
-            {items.map(todo => {
-                return (
-                    <li>{todo}</li>
-                )
-            })}
-        </ul>
-    )
-}
+
+const Todos: React.FC<PropsWithChildren<{ items: string[] }>> = ({
+  children,
+  items,
+}) => {
+  return (
+    <ul>
+      {items.map((todo) => {
+        return <li>{todo}</li>;
+      })}
+    </ul>
+  );
+};
 export default Todos;
