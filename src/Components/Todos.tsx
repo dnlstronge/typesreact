3c5d5edccc37f754e41307d05c5b498545071eaf
Dan Stronge <dnlstronge@gmail.com>
2023-04-16 15:10:51 +0100

    [x] - format

--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -3,32 +3,32 @@ import classes from "./Todos.module.css";
 
 import { PropsWithChildren } from "react";
 
-type itemsingle = {id: string,
-                text: string}[]
+type itemsingle = { id: string; text: string }[];
 
 const Todos: React.FC<PropsWithChildren<{ items: itemsingle }>> = ({
   children,
   items,
 }) => {
-
-  
   return (
     <div className={classes.container}>
       <ul className={classes.list}>
         {items.map((todo) => {
           const deleteItem = () => {
-            const deleteThis: any = document.getElementById(todo.id)
-            deleteThis.remove()
-          }
+            const deleteThis: any = document.getElementById(todo.id);
+            deleteThis.remove();
+          };
           return (
-          <div id={} className={classes.listitemcontainer}>
-          <li id={todo.text} className={classes.listItem}>{todo.text}</li>
-          <button onClick={deleteItem} className={classes.deletebutton}>x</button>
-          </div>)
+            <div id={todo.id} className={classes.listitemcontainer}>
+              <li id={todo.text} className={classes.listItem}>
+                {todo.text}
+              </li>
+              <button onClick={deleteItem} className={classes.deletebutton}>
+                x
+              </button>
+            </div>
+          );
         })}
-       
       </ul>
-      
     </div>
   );
 };
