420369412255eceb7119c19f44c215042f6d8c22
Dan Stronge <dnlstronge@gmail.com>
2023-04-17 17:28:31 +0100

    [x] - format

--- src/Components/Testing/ComponentToTest.tsx
+++ src/Components/Testing/ComponentToTest.tsx
@@ -1,19 +1,37 @@
-import classes from "./ComponentToTest.module.css"
-import React, { useState } from "react"
-
-
+import classes from "./ComponentToTest.module.css";
+import React, { ChangeEvent, useState } from "react";
 
 /* Goal - create a component to write some unit tests for  */
 
 /* [1] - create a simple counter component which increments/deincremnets */
 
-
 const ComponentToTest = () => {
-    const [counter, setCounter] = useState(0)
-    return (
-        <div className={classes.container}>
-            <p className={}></p>
-        </div>
-    )
-}
+  const [counter, setCounter] = useState(0);
+
+  /* Handlers */
+
+  const counterHandler = (e: any) => {
+    if (e.target.value === "plus") {
+      setCounter((prev) => {
+        return prev + 1;
+      });
+    } else {
+      setCounter((prev) => {
+        return prev - 1;
+      });
+    }
+  };
+
+  return (
+    <div className={classes.container}>
+      <p className={classes.counter}></p>
+      <button onClick={counterHandler} value={"plus"} className={classes.btn}>
+        +
+      </button>
+      <button onClick={counterHandler} value={"minus"} className={classes.btn}>
+        -
+      </button>
+    </div>
+  );
+};
 export default ComponentToTest;
