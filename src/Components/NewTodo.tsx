4bcc29ab1a4ddd5c842018e55c4f8965627f5a02
Dan Stronge <dnlstronge@gmail.com>
2023-04-19 08:05:34 +0100

    [x] - add new component to add item [x] - this time use form

---
+++ src/Components/NewTodo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import classes from "./AddItem.module.css"
+
+const NewTodo = () => {
+    return (
+        <form className={classes.container}></form>
+    )
+}
+export default NewTodo;
