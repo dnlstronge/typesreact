5d2238bbb8fca8650cd45443a7522bc482f3cf2a
Dan Stronge <dnlstronge@gmail.com>
2023-04-19 08:09:14 +0100

    [x] - create template

--- src/Components/NewTodo.tsx
+++ src/Components/NewTodo.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import classes from "./AddItem.module.css"
 
 const NewTodo = () => {
+
+    const onSubmitHandler = () => {
+
+    }
     return (
-        <form className={classes.container}></form>
+        <form className={classes.container}>
+            <input type="text" className={classes.input}/>
+            <button className={classes.btn}>Add Item</button>
+        </form>
+
     )
 }
 export default NewTodo;
