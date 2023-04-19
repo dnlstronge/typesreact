5a4720a24cf0d5ce592c896b47d6aa470bc8edce
Dan Stronge <dnlstronge@gmail.com>
2023-04-19 12:02:11 +0100

    [x] - format

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,22 @@ import NewTodo from "./Components/NewTodo";
 
 /* figure out id (unique) for delete */
 
-import DataType from "./Components/Models/dataType"
+import DataType from "./Components/Models/dataType";
 
 function App() {
-
   /* Error */
-  const [error, setError] = useState(false) 
+  const [error, setError] = useState(false);
 
   /* Dummy data, created using type class and constructer which
   accepts text parameter */
 
   const [data, setData] = useState([
     new DataType("walk the cat"),
-    new DataType("Bounce off the walls")
-  ])
+    new DataType("Bounce off the walls"),
+  ]);
 
   /* dummy data II */
-  
+
   const [items, setItems] = useState([
     {
       id: "walk dog",
@@ -38,41 +37,38 @@ function App() {
     },
   ]);
 
-  type itemsArray = {id: string, text: string}[]
+  type itemsArray = { id: string; text: string }[];
 
+  /* updates dummy data II - (redundant) */
 
-  /* updates dummy data II - (redundant) */ 
-
-  const addItem = (itemToAdd: string, ...jobs: { id: string, text: string }[]) => {
+  const addItem = (
+    itemToAdd: string,
+    ...jobs: { id: string; text: string }[]
+  ) => {
     let str: string = itemToAdd;
-    setItems((prev: itemsArray) => { 
-      return [...prev, {id: str, text: str}]
-    
-  })
-}
+    setItems((prev: itemsArray) => {
+      return [...prev, { id: str, text: str }];
+    });
+  };
 
   /* updates todo list */
 
   const newAddItem = (itemToAdd: string, ...data: DataType[]) => {
-   if(itemToAdd.length > 0) {
-    setError(false)
-    let str: string = itemToAdd;
-    setData((prev) => {
-      return [...prev, new DataType(str)]
-    })
-   } else {
-    setError(true)
-    console.log("error please enter a value")
-   }
-    
-  }
+    if (itemToAdd.length > 0) {
+      setError(false);
+      let str: string = itemToAdd;
+      setData((prev) => {
+        return [...prev, new DataType(str)];
+      });
+    } else {
+      setError(true);
+    }
+  };
 
-  
   return (
     <div className={classes.app}>
       <AddItem updateError={setError} addItem={newAddItem} />
-      {error && 
-      <p className={classes.errormsg}>Task cannot be empty</p>}
+      {error && <p className={classes.errormsg}>Task cannot be empty</p>}
       <NewTodo updateError={setError} addItem={newAddItem} />
       <Todos items={data} />
       <ComponentToTest />
