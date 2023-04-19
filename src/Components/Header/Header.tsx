c0caa5a180a9f77e73fa9b2f9ab3e7b3a69c3e39
Dan Stronge <dnlstronge@gmail.com>
2023-04-19 13:21:07 +0100

    [x] - format & check sections hide/show

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -9,25 +9,28 @@ const Header: React.FC<{
     }>
   >;
 }> = ({ show }) => {
+  /* handlers */
 
-    /* handlers */ 
-
-    const handleshowA = () => {
-        show((prev) => {
-           return {show_A: true, show_B: false}
-        })
-    }
-    const handleshowB = () => {
-        show((prev) => {
-            return { show_A: false, show_B: true }
-        })
-    }
+  const handleshowA = () => {
+    show((prev) => {
+      return { show_A: true, show_B: false };
+    });
+  };
+  const handleshowB = () => {
+    show((prev) => {
+      return { show_A: false, show_B: true };
+    });
+  };
   return (
     <header className={classes.heading}>
-      <p className={classes.title}>Playground App</p>
+      <p className={classes.title}>Playground</p>
       <div className={classes.btncontainer}>
-        <button onClick={handleshowA} className={classes.btn}>Section A</button>
-        <button onClick={handleshowB} className={classes.btn}>Section B</button>
+        <button onClick={handleshowA} className={classes.btn}>
+          Section A
+        </button>
+        <button onClick={handleshowB} className={classes.btn}>
+          Section B
+        </button>
       </div>
     </header>
   );
