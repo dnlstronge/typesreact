6e55b8124aaf2f54fa488036fc9ea394215537c4
Dan Stronge <dnlstronge@gmail.com>
2023-04-20 15:07:42 +0100

    [x] - add css

--- src/Content/Projects/grouptask.ts
+++ src/Content/Projects/grouptask.ts
@@ -1,6 +1,8 @@
 const grouptaskcontent = {
     main: `Project management app built with React, allows different users to log in and
-     exchange messages, ideas and update tasks etc. Uses firebase for Auth management and to accommodate CRUD operations`
+     exchange messages, ideas and update tasks etc. Uses firebase for Auth management 
+     and to accommodate CRUD operations. Users can create an account and log-in, they can view 
+     tasks based on different filters and whether they are assigned to them.`
 }
 
 export default grouptaskcontent;
