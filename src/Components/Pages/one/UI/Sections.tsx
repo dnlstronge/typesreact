c66a4858d0602e37b7ea290fe46d4c4492d469d2
Dan Stronge <dnlstronge@gmail.com>
2023-04-20 19:47:37 +0100

    [x] - add css

--- src/Components/Pages/one/UI/Sections.tsx
+++ src/Components/Pages/one/UI/Sections.tsx
@@ -15,11 +15,12 @@ content: points to content: datasource: string
 */
 
 const Sections: React.FC<project> = (props) => {
-
   /*local state */
   const [showVid, setShowvid] = useState(false);
   const [showSub, setShowSub] = useState(false);
   const [showAbout, setShowAbout] = useState(false);
+  const [imageClicked, setImageClicked] = useState(false)
+
 
   /* Handlers */
   const showVidHandler = () => {
@@ -35,17 +36,29 @@ const Sections: React.FC<project> = (props) => {
     setShowAbout(!showAbout);
   };
 
+  /* enlarge Image */
+  const enlargeHandler = (id: string) => {
+    setImageClicked(!imageClicked)
+   if(!imageClicked) {
+    const element = document.getElementById(id)!
+    element.className = classes.showimageenlarged
+   }
+   else {
+    const element = document.getElementById(id)!
+    element.className = classes.showimagemain
+   }
+  }
+
   /* reset */
-  
 
   /* animations (p) */
   const mounted = showSub ? classes.contentfoldertrans : classes.contentfolder;
   const mainimage = showSub ? classes.showimagemain : classes.imagemain;
   const about = showAbout ? classes.aboutactive : classes.aboutinactive;
+  
 
   return (
     <section className={classes.subsection}>
-    
       <button onClick={showSubHandler} className={classes.btn}>
         {props.title}
       </button>
@@ -55,9 +68,19 @@ const Sections: React.FC<project> = (props) => {
           <img className={mainimage} src={props.image} alt={props.title} />
           {props.imageArray.map((image) => {
             return (
-              <img className={mainimage} src={image} alt="screen shot of app" />
+              <img
+                onClick={() => {
+                  enlargeHandler(image);
+                }}
+                key={image}
+                id={image}
+                className={mainimage}
+                src={image}
+                alt="screen shot of app"
+              />
             );
           })}
+           
           {showVid && showSub && (
             <ReactPlayer
               className={classes.player}
@@ -65,7 +88,9 @@ const Sections: React.FC<project> = (props) => {
               controls={true}
             />
           )}
-          {showAbout && showSub && <p className={about}>{props.contentMain}</p>}
+          {showSub && (
+            <p className={about}>{showAbout ? props.contentMain : ""}</p>
+          )}
 
           {showSub && (
             <div className={classes.nav}>
@@ -93,8 +118,6 @@ const Sections: React.FC<project> = (props) => {
               </div>
             </div>
           )}
-
-          <p className={classes.content}>{props.contentMain}</p>
         </section>
       </div>
     </section>
