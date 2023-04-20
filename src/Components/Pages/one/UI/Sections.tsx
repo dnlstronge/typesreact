52c9c48dffc8f0c5ba0d175a88fc925f676fa829
Dan Stronge <dnlstronge@gmail.com>
2023-04-20 14:58:45 +0100

    [x] - add css

--- src/Components/Pages/one/UI/Sections.tsx
+++ src/Components/Pages/one/UI/Sections.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 import classes from "./Sections.module.css";
 import project from "../../../../Models/Projects";
+import { Link } from "react-router-dom";
 /* Testing (sections.test.tsx) */
 
 /* <Config>
@@ -14,30 +15,34 @@ content: points to content: datasource: string
 */
 
 const Sections: React.FC<project> = (props) => {
-
   /*local state */
-  const [showVid, setShowvid] = useState(false)
+  const [showVid, setShowvid] = useState(false);
   const [showSub, setShowSub] = useState(false);
-  const [showAbout, setShowAbout] = useState(false)
+  const [showAbout, setShowAbout] = useState(false);
+  
 
   /* Handlers */
   const showVidHandler = () => {
-    setShowAbout(false)
-    setShowvid(!showVid)
-  }
+    setShowAbout(false);
+    setShowvid(!showVid);
+  };
 
   const showSubHandler = () => {
     setShowSub(!showSub);
   };
   const showAboutHandler = () => {
-    setShowvid(false)
-    setShowAbout(!showAbout)
-  }
+    setShowvid(false);
+    setShowAbout(!showAbout);
+  };
+
+  /* reset */
+
+  
 
   /* animations (p) */
   const mounted = showSub ? classes.contentfoldertrans : classes.contentfolder;
   const mainimage = showSub ? classes.showimagemain : classes.imagemain;
-  const about = showAbout ? classes.aboutactive : classes.aboutinactive
+  const about = showAbout ? classes.aboutactive : classes.aboutinactive;
 
   return (
     <section className={classes.subsection}>
@@ -46,28 +51,40 @@ const Sections: React.FC<project> = (props) => {
       </button>
 
       <div className={mounted}>
-       
-          <section>
-            <img className={mainimage} src={props.image} alt={props.title}/>
-            {props.imageArray.map((image) => {
-              return <img className={mainimage} src={image} alt="screen shot of app"/>
-            })}
-            {showVid && showSub &&
-            <ReactPlayer className={classes.player} url={props.gif} controls={true}/>}
-            {showAbout && showSub &&
-            <p className={about}>{props.contentMain}</p> }
+        <section>
+          <img className={mainimage} src={props.image} alt={props.title} />
+          {props.imageArray.map((image) => {
+            return (
+              <img className={mainimage} src={image} alt="screen shot of app" />
+            );
+          })}
+          {showVid && showSub && (
+            <ReactPlayer
+              className={classes.player}
+              url={props.gif}
+              controls={true}
+            />
+          )}
+          {showAbout && showSub && <p className={about}>{props.contentMain}</p>}
 
-            {showSub && 
+          {showSub && (
             <div className={classes.nav}>
-              <button className={classes.navbtn} onClick={showVidHandler}>Video</button>
-              <a href={props.url} className={classes.navbtn}>Live demo</a>
-              <a href={props.git}className={classes.navbtn}>Github</a>
-              <button className={classes.navbtn} onClick={showAboutHandler}>About</button>
-            </div>}
-            
-            <p className={classes.content}>{props.contentMain}</p>
-          </section>
-     
+              <button className={classes.navbtn} onClick={showVidHandler}>
+                Video
+              </button>
+              <button className={classes.navbtn} onClick={showAboutHandler}>
+                About
+              </button>
+              <div className={classes.links}>
+                <a className={classes.navanchor} target="blank" href={props.git}>Github</a>
+                <a className={classes.navanchor} target="blank" href={props.url}>Live demo</a>
+              </div>
+              
+            </div>
+          )}
+
+          <p className={classes.content}>{props.contentMain}</p>
+        </section>
       </div>
     </section>
   );
