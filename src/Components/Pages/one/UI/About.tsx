2f9ce86100b33f2c05bcc5591f149b917063f1b7
Dan Stronge <dnlstronge@gmail.com>
2023-04-21 10:36:45 +0100

    [x] - add mor icons to map

--- src/Components/Pages/one/UI/About.tsx
+++ src/Components/Pages/one/UI/About.tsx
@@ -1,22 +1,22 @@
+import React from "react";
+import classes from "./About.module.css";
+import { FaReact } from "react-icons/fa";
+import { BsFiletypeCss } from "react-icons/bs";
+import { IoLogoJavascript } from "react-icons/io";
+import { TbBrandTypescript } from "react-icons/tb";
+import { AiOutlineHtml5 } from "react-icons/ai";
 
-import React from "react"
-import classes from "./About.module.css"
-import { FaReact } from "react-icons/fa"
-import { BsFiletypeCss } from "react-icons/bs"
-
-
+import {} from "react-icons/";
 
 /* Map for all icons */
 
-const icons= { 
-
-    react: <FaReact className={classes.reacticon}/>,
-    css: <BsFiletypeCss className={classes.cssicon}/>,
-    html5: "",
-    typescript: "",
-    javascript: "",
-
-}
+const icons = {
+  react: <FaReact className={classes.reacticon} />,
+  css: <BsFiletypeCss className={classes.cssicon} />,
+  html5: <AiOutlineHtml5 className={classes.htmlicon}/>,
+  typescript: <TbBrandTypescript className={classes.tsicon}/>,
+  javascript: <IoLogoJavascript className={classes.jsicon} />,
+};
 
 /* config: 
 
@@ -26,19 +26,16 @@ icons: array of strings (checked with icons map to render icons)
 req: react ICONS
 */
 
-
-
-const About: React.FC<{content: string}> = (props) => {
-   return ( <div>
-
-        {/*content main */}
-        <p>{props.content}</p>
-        {icons.react}
-        {/*content sup */}
-        <p>
-        </p>
-        <section className={classes.icons }></section>
+const About: React.FC<{ content: string }> = (props) => {
+  return (
+    <div>
+      {/*content main */}
+      <p>{props.content}</p>
+      {icons.react}
+      {/*content sup */}
+      <p></p>
+      <section className={classes.icons}></section>
     </div>
-   )
-}
+  );
+};
 export default About;
