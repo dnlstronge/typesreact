8d5227e805ac2b8835c332c03ddf7164dd8e0942
Dan Stronge <dnlstronge@gmail.com>
2023-04-21 09:24:12 +0100

    [x] - add scripting for media player

--- src/Components/Pages/one/UI/Sections.tsx
+++ src/Components/Pages/one/UI/Sections.tsx
@@ -19,14 +19,38 @@ const Sections: React.FC<project> = (props) => {
   const [showVid, setShowvid] = useState(false);
   const [showSub, setShowSub] = useState(false);
   const [showAbout, setShowAbout] = useState(false);
-  const [imageClicked, setImageClicked] = useState(false)
-
+  const [imageClicked, setImageClicked] = useState(false);
 
   /* Handlers */
   const showVidHandler = () => {
+    const snap = document.getElementById("videobtn")!
+    snap.scrollIntoView();
     setShowAbout(false);
     setShowvid(!showVid);
-  };
+    if(showVid) {
+  
+    const element = document.getElementById("mediaplayer")!;
+    element.style.cssText = `
+          height: 428px;
+          width: 640px;
+          margin-left: 20%;
+          margin-right: 20%;
+          border: 2rem; solid black;
+          transition: all 2s ease-in-out;
+          transition: width 2s, height 3s;`;
+  } else {
+    const element = document.getElementById("mediaplayer")!;
+    element.style.cssText = `
+          height: 0px;
+          width: 640px;
+          margin-left: 20%;
+          margin-right: 20%;
+          border: 2rem; solid black;
+          transition: all 1s ease-in-out;
+          transition: width 1s, height 2s;`;
+
+  }
+}
 
   const showSubHandler = () => {
     setShowSub(!showSub);
@@ -38,18 +62,17 @@ const Sections: React.FC<project> = (props) => {
 
   /* enlarge Image */
   const enlargeHandler = (id: string) => {
-    setImageClicked(!imageClicked)
-   if(imageClicked) {
-    const element = document.getElementById(id)!
-    element.className = classes.showimageenlarged
-    const snapto = document.getElementById("snapto")!
-    snapto.scrollIntoView();
-   }
-   else {
-    const element = document.getElementById(id)!
-    element.className = classes.showimagemain
-   }
-  }
+    setImageClicked(!imageClicked);
+    if (imageClicked) {
+      const element = document.getElementById(id)!;
+      element.className = classes.showimageenlarged;
+      const snapto = document.getElementById("snapto")!;
+      snapto.scrollIntoView();
+    } else {
+      const element = document.getElementById(id)!;
+      element.className = classes.showimagemain;
+    }
+  };
 
   /* reset */
 
@@ -58,40 +81,40 @@ const Sections: React.FC<project> = (props) => {
   const mainimage = showSub ? classes.showimagemain : classes.imagemain;
   const about = showAbout ? classes.aboutactive : classes.aboutinactive;
   const snapto = showSub ? classes.snaptotrans : classes.snapto;
-  
 
   return (
     <>
-    <section className={classes.subsection}>
-      <button onClick={showSubHandler} className={classes.btn}>
-        {props.title}
-      </button>
-      
-      <div className={mounted}>
+      <section className={classes.subsection}>
+        <button onClick={showSubHandler} className={classes.btn}>
+          {props.title}
+        </button>
 
-         {/* images */}
-        <section className={classes.imageSection}>
-          <img className={mainimage} src={props.image} alt={props.title} />
-          {props.imageArray.map((image) => {
-            return (
-              <img
-                onClick={() => {
-                  enlargeHandler(image);
-                }}
-                key={image}
-                id={image}
-                className={mainimage}
-                src={image}
-                alt="screenshot of app"
-              />
-            );
-          })}
-           </section>
+        <div className={mounted}>
+          {/* images */}
+          <section className={classes.imageSection}>
+            <img className={mainimage} src={props.image} alt={props.title} />
+            {props.imageArray.map((image) => {
+              return (
+                <img
+                  onClick={() => {
+                    enlargeHandler(image);
+                  }}
+                  key={image}
+                  id={image}
+                  className={mainimage}
+                  src={image}
+                  alt="screenshot of app"
+                />
+              );
+            })}
+          </section>
 
           {/* Media Player */}
-          {showVid && showSub && (
+          {showSub && (
             <ReactPlayer
-              className={classes.player}
+              id="mediaplayer"
+              height="0"
+              width="50%"
               url={props.gif}
               controls={true}
             />
@@ -106,39 +129,36 @@ const Sections: React.FC<project> = (props) => {
 
           {showSub && (
             <section className={classes.navbuttons}>
-            <div className={classes.nav}>
-              <button className={classes.navbtn} onClick={showVidHandler}>
-                Video
-              </button>
-              <button className={classes.navbtn} onClick={showAboutHandler}>
-                About
-              </button>
-              <div className={classes.links}>
-                <a
-                  className={classes.navanchor}
-                  target="blank"
-                  href={props.git}
-                >
-                  Github
-                </a>
-                <a
-                  className={classes.navanchor}
-                  target="blank"
-                  href={props.url}
-                >
-                  Live demo
-                </a>
+              <div className={classes.nav}>
+                <button id="videobtn" className={classes.navbtn} onClick={showVidHandler}>
+                  Video
+                </button>
+                <button className={classes.navbtn} onClick={showAboutHandler}>
+                  About
+                </button>
+                <div className={classes.links}>
+                  <a
+                    className={classes.navanchor}
+                    target="blank"
+                    href={props.git}
+                  >
+                    Github
+                  </a>
+                  <a
+                    className={classes.navanchor}
+                    target="blank"
+                    href={props.url}
+                  >
+                    Live demo
+                  </a>
+                </div>
               </div>
-            </div>
-            
             </section>
           )}
-        
-      </div>
-      <section className={snapto} id="snapto"></section>
+        </div>
+        <section className={snapto} id="snapto"></section>
       </section>
-      
-      </>
+    </>
   );
 };
 
