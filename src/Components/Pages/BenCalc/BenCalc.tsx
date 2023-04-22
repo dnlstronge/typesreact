fbcf8e3d5f659515a835dfcbb1da71183fe6b240
Dan Stronge <dnlstronge@gmail.com>
2023-04-22 16:32:30 +0100

    [x] - create template

--- src/Components/Pages/BenCalc/BenCalc.tsx
+++ src/Components/Pages/BenCalc/BenCalc.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-
+import classes from "BenCalcLoader.module.css"
 
 const BenCalc = () => {
+    return (
+        <div className={classes.container}>
+            <nav>
+                <button>Programs</button>
+                <button>Resources</button>
+                <button>Info</button>
+            </nav>
+            
+            {/* components for each section to follow */}
 
+        </div>
+    )
 }
 
 export default BenCalc; 
