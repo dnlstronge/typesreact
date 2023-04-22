4553af0a7945e1a203c9890b056ae4fa46dee466
Dan Stronge <dnlstronge@gmail.com>
2023-04-22 16:37:43 +0100

    [x] - set up transition animation

--- src/Components/Pages/BenCalc/BenCalc.tsx
+++ src/Components/Pages/BenCalc/BenCalc.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import classes from "BenCalcLoader.module.css"
+import classes from "BenCalcLoader.module.css";
+import { motion } from "framer-motion";
 
 const BenCalc = () => {
-    /* Local state */
+  /* Local state */
 
-    /* Handlers */
+  /* Handlers */
 
-    return (
-        <div className={classes.container}>
-            <nav className={classes.navbar}>
-                <button className={classes.navbtn}>Programs</button>
-                <button className={classes.navbtn}>Resources</button>
-                <button className={classes.navbtn}>Info</button>
-            </nav>
-            <section className={classes.section}></section>
-            {/* components for each section to follow */}
+  return (
+    <motion.div
+      className={classes.container}
+      initial={{ width: 0, background: "black" }}
+      animate={{ width: "100%", background: "black" }}
+      exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
+    >
+      <nav className={classes.navbar}>
+        <button className={classes.navbtn}>Programs</button>
+        <button className={classes.navbtn}>Resources</button>
+        <button className={classes.navbtn}>Info</button>
+      </nav>
+      <section className={classes.section}></section>
+      {/* components for each section to follow */}
+    </motion.div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default BenCalc; 
+export default BenCalc;
--- src/Routes/AnimationRoutes.tsx
+++ src/Routes/AnimationRoutes.tsx
@@ -6,6 +6,7 @@ import Projects from "../Components/Pages/Projects/Projects";
 
 import { AnimatePresence } from "framer-motion"
 import Playground from "../Components/Pages/Playground/Playground";
+import BenCalc from "../Components/Pages/BenCalc/BenCalc";
 
 const AnimationRoutes = () => {
   const location = useLocation();
@@ -13,6 +14,7 @@ const AnimationRoutes = () => {
     <AnimatePresence>
     <Routes location={location}  key={location.pathname}>
       <Route path="/" element={<Home />} />
+      <Route path="/bencalc" element = {<BenCalc />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/playground" element={<Playground/>} />
     </Routes>
