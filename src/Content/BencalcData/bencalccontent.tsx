62ecec3dcf08b5617bda38dd1dc0dbcb89888112
Dan Stronge <dnlstronge@gmail.com>
2023-04-23 10:37:23 +0100

    [x] - add updates array to content

--- src/Content/BencalcData/bencalccontent.tsx
+++ src/Content/BencalcData/bencalccontent.tsx
@@ -1,3 +1,15 @@
 const bencalcdata = {
-    p1: `Welcome to bencalc, the welfare rights toolkit`
+    p1: `Welcome to bencalc, the welfare rights toolkit`,
+
+    updates: [
+        "Rebuilt UI April 23",
+        "redeploy UC & PC recks - april 23",
+        "Major changes to UI implmented - March 23",
+        "Pension Credit reckoner has been updated = feb 23",
+        "Added UC reckoner - jan 23",
+        "manual tests of UC calc - need to add capital section"
+
+    ]
 }
+
+export default bencalcdata;
