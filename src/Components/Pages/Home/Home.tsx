f4f641ae6e57ad64b84e1b73a78dc5ce348dd7e9
Dan Stronge <dnlstronge@gmail.com>
2023-04-23 11:26:29 +0100

    [x] - change animation

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -1,61 +1,66 @@
 import React from "react";
 import classes from "./Home.module.css";
-import { useState } from "react"
+import { useState } from "react";
 import { motion } from "framer-motion";
 import Contact from "./Contact";
 
 import HomeContent from "./HomeContent";
 
 const Home = () => {
-
-const [showAbout, setShowAbout] = useState(false)
-const [showContact, setShowContact] = useState(false)
-
+  const [showAbout, setShowAbout] = useState(false);
+  const [showContact, setShowContact] = useState(false);
 
   /* Handlers  */
 
   const showAboutHandler = () => {
-    setShowContact(false)
-    setShowAbout(true)
-    const element = document.getElementById("focusdiv")!
-    element.scrollIntoView()
-  }
+    setShowContact(false);
+    setShowAbout(true);
+    const element = document.getElementById("focusdiv")!;
+    element.scrollIntoView();
+  };
 
   const showContactHandler = () => {
-    setShowAbout(false)
-    setShowContact(true)
-    const element = document.getElementById("focusdiv")!
-    element.scrollIntoView()
-  }
-
+    setShowAbout(false);
+    setShowContact(true);
+    const element = document.getElementById("focusdiv")!;
+    element.scrollIntoView();
+  };
 
   return (
     <motion.div
       className={classes.container}
       initial={{ width: 0, background: "black" }}
       animate={{ width: "100%", background: "black" }}
-      exit={{ x: window.innerWidth, transition: {duration: 0.1} }}
-    >   
-        <header className={classes.headsection}>
+      exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
+    >
+      <header className={classes.headsection}>
         <h4 className={classes.heading}>Home</h4>
-        
-      
       </header>
-      <div className={classes.buttons}> 
-        <button onClick={showAboutHandler} className={classes.aboutbtn}>About</button>
-        <button onClick={showContactHandler} className={classes.aboutbtn}>Contact</button> 
+      <div className={classes.buttons}>
+        <button onClick={showAboutHandler} className={classes.aboutbtn}>
+          About
+        </button>
+        <button onClick={showContactHandler} className={classes.aboutbtn}>
+          Contact
+        </button>
+      </div>
+
+      {showAbout && (
+        <motion.div    initial={{ width: "100%", background: "black", opacity: 0 }}
+        animate={{ width: "100%", opacity: 1, background: "black",   }}
+        exit={{ x: window.innerWidth,  display: "none", opacity: 1, transition: { duration: 0.9 } }}>
+          <HomeContent showAbout={showAbout} />
+        </motion.div>
+      )}
+      {showContact && (
+        <div>
+          {" "}
+          <Contact />
         </div>
-     
-      {showAbout && 
-        <HomeContent showAbout={showAbout} />}
-      {showContact &&
-        <Contact />
-      }
-        <div id="focusdiv"></div>
-     
-      
+      )}
+      <div id="focusdiv"></div>
+
       {/* add skills & tech */}
- 
     </motion.div>
   );
 };
--- src/Components/Pages/Home/HomeContent.tsx
+++ src/Components/Pages/Home/HomeContent.tsx
@@ -13,18 +13,12 @@ const HomeContent: React.FC<{ showAbout: boolean }> = ({ showAbout }) => {
       {showAbout && (
         <motion.div
           className={classes.abouttext}
-          initial={{ width: "80%", height: 0, background: "black", color: "black", opacity: 0 }}
-          animate={{ width: "80%", height: "20%", background: "black", color: "white", opacity: 1 }}
-          exit={{ y: window.outerHeight, transition: { duration: 4 } }}
         >
           {homeContent.main}<br></br>{homeContent.sup}
         </motion.div>
       )}
       {showAbout && 
       < motion.div
-       initial={{ width: 0, background: "black", }}
-       animate={{ width: "90%", background: "black", marginLeft:"4rem" }}
-       exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
       >
       <h3 className={classes.techHead}>Tech...</h3>
       <section className={classes.techused}>
