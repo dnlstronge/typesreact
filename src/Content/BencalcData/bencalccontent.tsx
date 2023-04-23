e365e1ff489b163f0e67ca2a95b388241c8cd08d
Dan Stronge <dnlstronge@gmail.com>
2023-04-23 11:00:06 +0100

    [x] - add content

--- src/Content/BencalcData/bencalccontent.tsx
+++ src/Content/BencalcData/bencalccontent.tsx
@@ -1,5 +1,6 @@
 const bencalcdata = {
-    p1: `Welfare rights advice toolkit and resource`,
+    p1: `Welfare rights advice toolkit, a resource for advice professionals based in the UK. Here you will 
+    find a series of quick-benefit calculators as well as other useful tools and resources`,
 
     updates: [
         "Rebuilt UI April 23",
