a82c8cad91fc0c3e5ac50bf39f93ec194227c556
Dan Stronge <dnlstronge@gmail.com>
2023-04-24 08:37:25 +0100

    [x] - fix typos

--- src/Content/Playground/playgroundContent.tsx
+++ src/Content/Playground/playgroundContent.tsx
@@ -1,8 +1,8 @@
 const playgroundContent = {
     p1: `
-        I've built these section as a place to try out new ideas or implment 
+        I've built this section as a place to try out new ideas or implement 
         new techniques. I've divided theses into different sections to showcase
-        by learning and development and to keep everything organised in the one place. 
+        my learning and development and to keep everything organised in one place. 
     `
 }
 
