f48a93097bf4921bdd67901cca96943e7313939a
Dan Stronge <dnlstronge@gmail.com>
2023-04-24 08:27:21 +0100

    [x] - tidy [x] - css changes

--- src/Components/Pages/Playground/Playground.tsx
+++ src/Components/Pages/Playground/Playground.tsx
@@ -1,58 +1,50 @@
-import React, { useState } from 'react'
-import classes from "./Playground.module.css"
-import { motion } from 'framer-motion';
-import playgroundContent from '../../../Content/Playground/playgroundContent';
-import PGNav from './PG-Nav/PGNav';
-import { FaSleigh } from 'react-icons/fa';
+import React, { useState } from "react";
+import classes from "./Playground.module.css";
+import { motion } from "framer-motion";
+import playgroundContent from "../../../Content/Playground/playgroundContent";
+import PGNav from "./PG-Nav/PGNav";
+import { FaSleigh } from "react-icons/fa";
 
- function Playground() {
-    const [showApis, setShowApis] = useState(false)
-    const [showTrivial, setShowTrivial] = useState(false)
-    const [showMisc, setShowMisc] = useState(false)
+function Playground() {
+  const [showApis, setShowApis] = useState(false);
+  const [showTrivial, setShowTrivial] = useState(false);
+  const [showMisc, setShowMisc] = useState(false);
 
+  /* handlers */
 
+  const showApisHandler = () => {
+    setShowApis(true);
+    setShowTrivial(false);
+    setShowMisc(false);
+  };
 
-
-
-    /* handlers */
-
-    const showApisHandler = () => {
-        setShowApis(true)
-        setShowTrivial(false)
-        setShowMisc(false)
-    }
-
-    const showTrivialHandler = () => {
-        setShowApis(false)
-        setShowTrivial(true)
-        setShowMisc(false)
-    }
-    const showMiscHandler = () => {
-      setShowApis(false)
-        setShowTrivial(false)
-        setShowMisc(true)
-    }
+  const showTrivialHandler = () => {
+    setShowApis(false);
+    setShowTrivial(true);
+    setShowMisc(false);
+  };
+  const showMiscHandler = () => {
+    setShowApis(false);
+    setShowTrivial(false);
+    setShowMisc(true);
+  };
   return (
     <motion.div
-    className={classes.container}
-    initial={{ width: 0 }}
-    animate={{ width: "100%" }}
-    exit={{ x: window.innerWidth, transition: {duration: 0.1} }}
-  >
+      className={classes.container}
+      initial={{ width: 0 }}
+      animate={{ width: "100%" }}
+      exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
+    >
       <header className={classes.headsection}>
-      <h4 className={classes.heading}>Playground</h4>
-      <p className={classes.mainpara}>
-        {playgroundContent.p1}
-      </p>
-      {/* Custom nav to follow */}
-     <PGNav />
-      <div className={classes.content}> 
+        <h4 className={classes.heading}>Playground</h4>
+      </header>
 
-      </div>
-    
-    </header>
-  </motion.div>
-  )
+      <p className={classes.mainpara}>{playgroundContent.p1}</p>
+      {/* Custom nav to follow */}
+      <PGNav />
+      <div className={classes.content}></div>
+    </motion.div>
+  );
 }
 
 export default Playground;
