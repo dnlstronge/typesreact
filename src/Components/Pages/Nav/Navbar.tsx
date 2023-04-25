5e1982a0964740400bff085d2d274708a45f15d8
Dan Stronge <dnlstronge@gmail.com>
2023-04-25 07:30:00 +0100

    [x] - add context

--- src/Components/Pages/Nav/Navbar.tsx
+++ src/Components/Pages/Nav/Navbar.tsx
@@ -2,11 +2,32 @@ import React from "react";
 import classes from "./Navbar.module.css"
 import { Link } from "react-router-dom";
 import NavButton from "./NavButton";
-
+import { Context } from "../../Context/Context";
 /** thinking of seting up context api for this - will see */
 
 const Navbar = () => {
 
+
+
+    /* handlers - updates context */
+
+    const setHome = () => {
+        Context.Provider = {
+            home: true,
+            programs: false,
+            playground: false,
+            about: false
+        }
+    }
+    const setPrograms = () => {
+
+    }
+    const setPlayground = () => {
+
+    }
+    const setAbout = () => {
+
+    }
 
     return (
         <nav className={classes.nav}>
