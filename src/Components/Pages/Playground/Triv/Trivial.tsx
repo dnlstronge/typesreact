e32bbae27a70c0e042adda4e434f870393eb7288
Dan Stronge <dnlstronge@gmail.com>
2023-04-26 11:05:24 +0100

    [x] - add link

--- src/Components/Pages/Playground/Triv/Trivial.tsx
+++ src/Components/Pages/Playground/Triv/Trivial.tsx
@@ -7,7 +7,7 @@ const Trivial = () => {
     return (
         <div className={classes.container}>
             <section className={classes.section}>
-                <a className={classes.anchor} href="" target="blank">Fetching website</a>
+                <a className={classes.anchor} href="https://apifetch-5ea29.web.app/" target="blank">Fetching website</a>
                 <p className={classes.para}>Simple website I built to try out fetching data form different apis, then 
                     manipulating the data in various ways
                 </p>
