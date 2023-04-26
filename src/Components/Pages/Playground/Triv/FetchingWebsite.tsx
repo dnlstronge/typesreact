d42d99cabfea9fddfacd5d57f4382d99868dbb0c
Dan Stronge <dnlstronge@gmail.com>
2023-04-26 11:36:33 +0100

    [x] - add css

--- src/Components/Pages/Playground/Triv/FetchingWebsite.tsx
+++ src/Components/Pages/Playground/Triv/FetchingWebsite.tsx
@@ -3,7 +3,23 @@
 
 
 const FetchingWebsite = () => {
-
+    return (
+        <div className={classes.container}>
+        <section className={classes.section}>
+          <a
+            className={classes.anchor}
+            href="https://apifetch-5ea29.web.app/"
+            target="blank"
+          >
+            Fetching website
+          </a>
+          <p className={classes.para}>
+            Simple website I built to try out fetching data form different apis,
+            then manipulating the data in various ways
+          </p>
+        </section>
+      </div>
+    )
 }
 
 export default FetchingWebsite;
--- src/Components/Pages/Playground/Triv/Trivial.tsx
+++ src/Components/Pages/Playground/Triv/Trivial.tsx
@@ -4,21 +4,7 @@ import classes from "./Trivial.module.css";
 const Trivial = () => {
   return (
     <>
-      <div className={classes.container}>
-        <section className={classes.section}>
-          <a
-            className={classes.anchor}
-            href="https://apifetch-5ea29.web.app/"
-            target="blank"
-          >
-            Fetching website
-          </a>
-          <p className={classes.para}>
-            Simple website I built to try out fetching data form different apis,
-            then manipulating the data in various ways
-          </p>
-        </section>
-      </div>
+     
 
       <div className={classes.container}>
         <section className={classes.section}>
