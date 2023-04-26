8e816f32720bfaebf25298a4ee9a1bb8a21983b4
Dan Stronge <dnlstronge@gmail.com>
2023-04-26 13:50:15 +0100

    [x] - format

--- src/Components/Pages/Playground/Playground.tsx
+++ src/Components/Pages/Playground/Playground.tsx
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import classes from "./Playground.module.css";
 import { motion } from "framer-motion";
-import playgroundContent from "../../../Content/Playground/playgroundContent";
 import PGNav from "./PG-Nav/PGNav";
 import Trivial from "./Triv/Trivial";
 import Misc from "./Misc/Misc";
 
-
 function Playground() {
-  
   const [showApis, setShowApis] = useState(false);
   const [showTrivial, setShowTrivial] = useState(false);
   const [showMisc, setShowMisc] = useState(false);
@@ -25,44 +22,46 @@ function Playground() {
     setShowApis(false);
     setShowTrivial(!showTrivial);
     setShowMisc(false);
-    const element = document.getElementById("snaptocontent")!
-    element.scrollIntoView()
+    const element = document.getElementById("snaptocontent")!;
+    element.scrollIntoView();
   };
   const showMiscHandler = () => {
     setShowApis(false);
     setShowTrivial(false);
-    setShowMisc(true);
-    const element = document.getElementById("snaptocontent")!
-    element.scrollIntoView()
+    setShowMisc(!true);
+    const element = document.getElementById("snaptocontent")!;
+    element.scrollIntoView();
   };
   return (
     <motion.div
       className={classes.container}
       initial={{ width: 0 }}
       animate={{ width: "100%" }}
-      exit={{ x: window.innerWidth, display: "none" ,transition: { duration: 0.1 } }}
+      exit={{
+        x: window.innerWidth,
+        display: "none",
+        transition: { duration: 0.1 },
+      }}
     >
       <header className={classes.headsection}>
         <h4 className={classes.heading}>Playground</h4>
       </header>
 
-      <motion.p 
-      className={classes.mainpara}
+      <motion.p
+        className={classes.mainpara}
         initial={{ width: "0%", background: "black", opacity: 0 }}
-        animate={{ width: "100%", opacity: 1, background: "black",   }}
-        exit={{ x: window.innerWidth, opacity: 0, transition: { duration: 0.3 } }}
-      
-      >
-        </motion.p>
-     
+        animate={{ width: "100%", opacity: 1, background: "black" }}
+        exit={{
+          x: window.innerWidth,
+          opacity: 0,
+          transition: { duration: 0.3 },
+        }}
+      ></motion.p>
+
       <PGNav miscClick={showMiscHandler} onClick={showTrivialHandler} />
       <div className={classes.content}>
-      {showTrivial && 
-        <Trivial />}
-      {showMisc && 
-        <Misc />
-        }
-       
+        {showTrivial && <Trivial />}
+        {showMisc && <Misc />}
       </div>
       <div id="snaptocontent"></div>
     </motion.div>
