96694f23233a05b0d8cdee505d26935dc2bcd6cb
Dan Stronge <dnlstronge@gmail.com>
2023-04-27 09:55:52 +0100

    [x] - add handler

--- src/Components/Pages/Playground/Triv/Sites/Websites.tsx
+++ src/Components/Pages/Playground/Triv/Sites/Websites.tsx
@@ -3,18 +3,40 @@ import Trivial from "../Trivial";
 import classes from "./Websites.module.css"
 
 const Websites = () => {
-    const [pageOne, setPage] = useState(true)
+    const [pageOne, setPageOne] = useState(true)
     const [pageTwo, setPageTwo] = useState(false)
     const [pageThree, setPageThree] = useState(false)
 
 
     /* handlers */
+
+    const handlePages = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        if(e.currentTarget.value === "one") {
+            setPageOne(true)
+            setPageTwo(false)
+            setPageThree(false)
+        }
+        if(e.currentTarget.value === "two") {
+            setPageOne(false)
+            setPageTwo(true)
+            setPageThree(false)
+        }
+        if(e.currentTarget.value === "three") {
+            setPageOne(false)
+            setPageTwo(false)
+            setPageThree(true)
+        }
+       
+    }
+
+    /**styles */
     return (
         <>
         <div className={classes.btncontainer}>
-            <button className={classes.btn}>1</button>
-            <button className={classes.btn}>2</button>
-            <button className={classes.btn}>3</button>
+            <button value="one" className={classes.btn}>1</button>
+            <button onClick={handlePages} value="two" className={classes.btn}>2</button>
+            <button value="three" className={classes.btn}>3</button>
         </div>
         <section className={classes.applications}>
             <Trivial />
