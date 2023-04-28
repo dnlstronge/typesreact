fc1aa171acdd1e162889724064c4d5c5d2e1dbac
Dan Stronge <dnlstronge@gmail.com>
2023-04-28 09:46:31 +0100

    [x] - add further inputs to form

--- src/Components/Pages/CaseWrite/CaseWrite.tsx
+++ src/Components/Pages/CaseWrite/CaseWrite.tsx
@@ -25,20 +25,33 @@ const CaseWrite = () => {
     )
     return (
         <div className={classes.container}>
-            <form>
+            <form className={classes.form}>
+               
+                <section className={classes.sections}>
+                <h2>CaseWrite</h2>
+                 <p className={classes.para}>Case note generator: Enter values about household/client and 
+                 click generate to compile a generic case note. Supplementary information can be 
+                 added in the text box at the end of form </p> 
+                </section>
                 {/* couple single?  */}
                 <section className={classes.sections}>
-                <label htmlFor="relationships">Relationship Status</label>
-                <select id="relationships">
+                <label className={classes.label} htmlFor="relationships">Relationship Status</label>
+                <select className={classes.select} id="relationships">
                     <option>Single</option>
                     <option>Couple</option>
                 </select>
                 </section>
                  {/* children */}
                 <section className={classes.sections}>
-                    <label htmlFor="children"></label>
-                    <input type="number"></input>
+                    <label className={classes.label} htmlFor="children"></label>
+                    <input className={classes.input} type="number"></input>
                 </section>
+                 {/* Benefit */}
+                 <section className={classes.section}>
+                    <label className={classes.label}></label>
+                    <input className={classes.input} type="text"></input>
+                 </section>
+                 {/* Disability benefit */}
 
             </form>
         </div>
