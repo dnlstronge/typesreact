b807172d8b421a92840412aa6d374de63a07cfe0
Dan Stronge <dnlstronge@gmail.com>
2023-04-28 12:39:32 +0100

    [x] - add logic [x] - format

--- src/Components/Pages/CaseWrite/CaseWrite.tsx
+++ src/Components/Pages/CaseWrite/CaseWrite.tsx
@@ -1,7 +1,6 @@
-import React, { FormEventHandler } from "react"
-import classes from "./CaseWrite.module.css"
-import { useState } from "react"
-
+import React, { FormEventHandler } from "react";
+import classes from "./CaseWrite.module.css";
+import { useState } from "react";
 
 /* Part of larger bencalc project - 
 
@@ -14,141 +13,159 @@ RNG select indexes....
 
 */
 
-/* data */ 
+/* data */
 
-import phrase from "./Data/phrases"
-
+import phrase from "./Data/phrases";
 
 /*helper function */
 
-// config -  length of array as an argument: 
+// config -  length of array as an argument:
 // will return 0 - (x - 1) as index
 
 const generateRandom = (x: number) => {
-    let random = Math.floor(Math.random() * x)
-    return random;
-}
+  let random = Math.floor(Math.random() * x);
+  return random;
+};
 const CaseWrite = () => {
-
-    /* to the right of each state value the number of phrases in the associated 
+  /* to the right of each state value the number of phrases in the associated 
     array is listed */
 
-    const [client, setClient] = useState(
-        {
-            single: false, // index 0 - 2
-            couple: false, // index 0 - 2
-            children: "", // log actual to string index 0 - 1
-            benefits: "", // list benefits IRO - index 0 
-            entitlement: "", // identified ent - index 0 - 
-            disability: "" // index 0 - 2
-        }
-    )
+  const [client, setClient] = useState({
+    single: false, // index 0 - 2
+    couple: false, // index 0 - 2
+    children: "", // log actual to string index 0 - 1
+    benefits: "", // list benefits IRO - index 0
+    entitlement: "", // identified ent - index 0 -
+    disability: "", // index 0 - 2
+  });
 
-    const [caseNote, setCaseNote] = useState("Initial Text value") // state to be used to render paragraph
-    const [caseNotetwo, setCaseNoteTwo] = useState("") // 2nd para - disability
+  const [caseNote, setCaseNote] = useState("Initial Text value"); // state to be used to render paragraph
+  const [caseNotetwo, setCaseNoteTwo] = useState(""); // 2nd para - disability
 
-/* Handlers  */ 
+  /* Handlers  */
 
+  /* submit (update state) */
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log("Woohoo form submitted");
+    e.preventDefault();
 
-/* submit (update state) */
-    const onSubmit = (e: React.FormEvent<HTMLFormElement> ) =>  {
-        console.log("Woohoo form submitted")
-       e.preventDefault()
-        console.log(client.single)
-        console.log(client.couple)
-        console.log(client.children)
-        console.log(client.benefits)
-        if(client.single) {
-            console.log("form has reached IF check")
-            setCaseNote(
-                `
+    console.log(client.single);
+    console.log(client.couple);
+    console.log(client.children);
+    console.log(client.benefits);
+    if (client.single) {
+      console.log("form has reached IF check");
+      setCaseNote(
+        `
                 ${phrase.single[generateRandom(3)]}. 
-                ${phrase.children[generateRandom(2)]}${client.children} dependents.
+                ${phrase.children[generateRandom(2)]}${
+          client.children
+        } dependents.
                 ${phrase.benefits[generateRandom(4)]}${client.benefits}.
 
                 `
-            )
-        }
-        if(client.couple) {
-            setCaseNote(
-                `
+      );
+    }
+    if (client.couple) {
+      setCaseNote(
+        `
                 ${phrase.couple[generateRandom(3)]}. 
-                ${phrase.children[generateRandom(2)]}${client.children} dependents.
+                ${phrase.children[generateRandom(2)]}${
+          client.children
+        } dependents.
                 ${phrase.benefits[generateRandom(4)]}${client.benefits}.
 
                 `
-                )
-        }
+      );
+    }
+  };
+
+  /* event handlers for inputs */
+
+  const handleSingle = (e: React.FormEvent<HTMLSelectElement>) => {
+    if (e.currentTarget.value === "single") {
+      return setClient({ ...client, single: true, couple: false });
     }
 
-/* event handlers for inputs */
-
-const handleSingle = (e: React.FormEvent <HTMLSelectElement>) => {
-    if(e.currentTarget.value === "single") {
-        setClient({...client, single: true, couple: false })
+    if (e.currentTarget.value === "couple") {
+      return setClient({ ...client, single: false, couple: true });
+    } else {
+      return setClient({ ...client, single: false, couple: false });
     }
+  };
 
-    if(e.currentTarget.value === "couple") {
-        setClient({...client, single: false, couple: true  })
-    }
-    else {
-        setClient({...client, single: false, couple: false })
-    }
-}
+  const handleChildren = (e: React.FormEvent<HTMLInputElement>) => {
+    setClient({ ...client, children: e.currentTarget.value });
+  };
 
-const handleChildren = (e: React.FormEvent <HTMLInputElement>) => {
-    setClient({...client, children: e.currentTarget.value})
-}
+  const handleBenefits = (e: React.FormEvent<HTMLInputElement>) => {
+    setClient({ ...client, benefits: e.currentTarget.value });
+  };
 
-const handleBenefits = (e: React.FormEvent <HTMLInputElement>) => {
-    setClient({...client, benefits: e.currentTarget.value})
-}
+  return (
+    <div className={classes.container}>
+      <form onSubmit={onSubmit} className={classes.form}>
+        <section className={classes.sections}>
+          <h2>CaseWrite</h2>
+          <p className={classes.para}>
+            Case note generator: Enter values about household/client and click
+            generate to compile a generic case note. Supplementary information
+            can be added in the text box at the end of form{" "}
+          </p>
+        </section>
+        {/* couple single?  */}
+        <section className={classes.sections}>
+          <label className={classes.label} htmlFor="relationships">
+            Relationship Status
+          </label>
+          <select
+            onChange={handleSingle}
+            className={classes.select}
+            id="relationships"
+          >
+            <option value="none">--select--</option>
+            <option value="single">Single</option>
+            <option value="couple">Couple</option>
+          </select>
+        </section>
+        {/* children */}
+        <section className={classes.sections}>
+          <label className={classes.label} htmlFor="children">
+            Enter Number of children
+          </label>
+          <input
+            onChange={handleChildren}
+            id="children"
+            className={classes.input}
+            type="number"
+          ></input>
+        </section>
+        {/* Benefit */}
+        <section className={classes.section}>
+          <label htmlFor="benefit" className={classes.label}>
+            Enter main benefit: e.g UC
+          </label>
+          <input
+            onChange={handleBenefits}
+            id="benefit"
+            className={classes.input}
+            type="text"
+          ></input>
+        </section>
+        {/* Disability benefit */}
+        <section className={classes.section}>
+          <label htmlFor="sup">
+            <input id="sup" className={classes.supplementaryText}></input>
+          </label>
+        </section>
+        <button type="submit">Generate</button>
+      </form>
+      <div className={classes.caseNote}>
+        <p className={classes.para}>{caseNote}</p>
+        <p className={classes.para}>{}</p>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-    return (
-        <div className={classes.container}>
-            <form onSubmit={onSubmit} className={classes.form}>
-               
-                <section className={classes.sections}>
-                <h2>CaseWrite</h2>
-                 <p className={classes.para}>Case note generator: Enter values about household/client and 
-                 click generate to compile a generic case note. Supplementary information can be 
-                 added in the text box at the end of form </p> 
-                </section>
-                {/* couple single?  */}
-                <section className={classes.sections}>
-                <label className={classes.label} htmlFor="relationships">Relationship Status</label>
-                <select onChange={handleSingle} className={classes.select} id="relationships">
-                    <option value="Single">Single</option>
-                    <option value="Couple">Couple</option>
-                </select>
-                </section>
-                 {/* children */}
-                <section className={classes.sections}>
-                    <label className={classes.label} htmlFor="children">Enter Number of children</label>
-                    <input onChange={handleChildren} id="children" className={classes.input} type="number"></input>
-                </section>
-                 {/* Benefit */}
-                 <section className={classes.section}>
-                    <label htmlFor="benefit" className={classes.label}>Enter main benefit: e.g UC</label>
-                    <input onChange={handleBenefits} id="benefit" className={classes.input} type="text"></input>
-                 </section>
-                 {/* Disability benefit */}
-                 <section className={classes.section}>
-                    <label htmlFor="sup">
-                        <input id="sup"className={classes.supplementaryText}></input>
-                    </label>
-                 </section>
-                <button type="submit">Generate</button>
-            </form>
-            <div className={classes.caseNote}>
-                <p className={classes.para}>{caseNote}</p>
-                <p className={classes.para}>{}</p>
-            </div>
-        </div>
-    )
-}
-
-export default CaseWrite
+export default CaseWrite;
