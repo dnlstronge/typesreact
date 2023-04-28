f11e1b13e03282c0938e9e27d32fc3076b185498
Dan Stronge <dnlstronge@gmail.com>
2023-04-28 09:27:31 +0100

    [x] - set up local state

--- src/Components/Pages/CaseWrite/CaseWrite.tsx
+++ src/Components/Pages/CaseWrite/CaseWrite.tsx
@@ -1,9 +1,36 @@
 import React from "react"
 import classes from "./CaseWrite.module.css"
 
+
+/* Part of larger bencalc project - 
+
+goal is to generate generic welfare advice casenotes.
+Notes returned should be specific to circumstances entered but also vary in wording\
+
+*/
+
 const CaseWrite = () => {
+
+    const [client, setclient] = useState(
+        {
+            single: "single adult",
+            couple: "couple",
+            children: 0,
+            benefit: "",
+            entitlement: ""
+        }
+
+    )
     return (
-        <div className={classes.container}></div>
+        <div className={classes.container}>
+            <form>
+                <label htmlFor="relationships">Relationship Status</label>
+                <select id="relationships">
+                    <option>Single</option>
+                    <option>Couple</option>
+                </select>
+            </form>
+        </div>
     )
 }
 
