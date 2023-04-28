7966bfef67fa355d4a3f30a0789531762353dee6
Dan Stronge <dnlstronge@gmail.com>
2023-04-28 09:19:37 +0100

    [x] - set up basic template

---
+++ src/Components/Pages/CaseWrite/CaseWrite.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import classes from "./CaseWrite.module.css"
+
+const CaseWrite = () => {
+    return (
+        <div className={classes.container}></div>
+    )
+}
+
+export default CaseWrite
