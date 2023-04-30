56885f60e66aa7ee6fe0e0419d09b03c9c266786
Dan Stronge <dnlstronge@gmail.com>
2023-04-30 08:30:52 +0100

    [x] - format

--- src/Components/Pages/CaseWrite/Sections/CurrentBenefit.tsx
+++ src/Components/Pages/CaseWrite/Sections/CurrentBenefit.tsx
@@ -1,21 +1,22 @@
-import React from "react"
-import classes from "./Relationship.module.css"
+import React from "react";
+import classes from "./Relationship.module.css";
 
-
-const CurrentBenefit: React.FC<{handleBenefits: (e: React.FormEvent<HTMLInputElement>) => void}> = (props) => {
-return (
+const CurrentBenefit: React.FC<{
+  handleBenefits: (e: React.FormEvent<HTMLInputElement>) => void;
+}> = (props) => {
+  return (
     <section className={classes.section}>
-          <label htmlFor="benefit" className={classes.label}>
-            Enter main benefit: e.g UC
-          </label>
-          <input
-            onChange={props.handleBenefits}
-            id="benefit"
-            className={classes.input}
-            type="text"
-          ></input>
-        </section>
-)
-}
+      <label htmlFor="benefit" className={classes.label}>
+        Enter main benefit: e.g UC
+      </label>
+      <input
+        onChange={props.handleBenefits}
+        id="benefit"
+        className={classes.input}
+        type="text"
+      ></input>
+    </section>
+  );
+};
 
 export default CurrentBenefit;
