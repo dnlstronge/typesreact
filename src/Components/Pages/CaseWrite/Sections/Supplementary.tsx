37bbb87bf4c78ca96b8d546f5fe4ac89e745130a
Dan Stronge <dnlstronge@gmail.com>
2023-05-01 09:27:57 +0100

    [x] - add typing

--- src/Components/Pages/CaseWrite/Sections/Supplementary.tsx
+++ src/Components/Pages/CaseWrite/Sections/Supplementary.tsx
@@ -1,6 +1,18 @@
-const Supplementary = () => {
-  <section className={classes.section}>
-    <label htmlFor="sup"></label>
-    <input  onChange={handleSupplementary}id="sup" className={classes.input}></input>
-  </section>;
+import classes from "./Relationship.module.css";
+
+const Supplementary: React.FC<{
+  handleSupplementary: (e: React.FormEvent<HTMLInputElement>) => void;
+}> = () => {
+  return (
+    <section className={classes.section}>
+      <label className={classes.label} htmlFor="sup"></label>
+      <input
+        onChange={handleSupplementary}
+        id="sup"
+        className={classes.inputSup}
+      ></input>
+    </section>
+  );
 };
+
+export default Supplementary;
