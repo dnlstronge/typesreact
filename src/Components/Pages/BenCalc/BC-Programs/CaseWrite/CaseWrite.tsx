d4dc3c4c512ad26e791e5d751e263bc712db7990
Dan Stronge <dnlstronge@gmail.com>
2023-05-02 10:34:52 +0100

    [x] - edit template

--- src/Components/Pages/BenCalc/BC-Programs/CaseWrite/CaseWrite.tsx
+++ src/Components/Pages/BenCalc/BC-Programs/CaseWrite/CaseWrite.tsx
@@ -7,12 +7,12 @@ import { GrNotes } from "react-icons/gr"
 const CaseWrite= () => {
     return (
         <div className={classes.container}>
-            <a href="https://pccalc-3dbcd.web.app/" target="blank">
+            <a href="/casewrite" target="blank">
             <GrNotes className={classes.icon} color="lightBlue" size="4rem"/>
             </a>
             <div className={classes.textsection}>
-                <h5 className={classes.texthead}> CaseWrite (coming soon)</h5>
-                <p className={classes.text}>  Casenote generation app, takes user input and returns a 
+                <h5 className={classes.texthead}> CaseWrite</h5>
+                <p className={classes.text}>  Casenote generation tool, takes user input and returns a 
                 generic and varied casenote. For use in case recording where a simple and concise case note is required</p>
                 </div>
         </div>
