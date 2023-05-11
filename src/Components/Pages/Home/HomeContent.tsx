3e4f6a05f58e2d6e0c8dddf8b983f8ae623e85c4
Dan Stronge <dnlstronge@gmail.com>
2023-05-11 07:25:49 +0100

    [x] - format

--- src/Components/Pages/Home/HomeContent.tsx
+++ src/Components/Pages/Home/HomeContent.tsx
@@ -4,29 +4,26 @@ import homeContent from "../../../Content/Home/homeContent";
 import { AnimatePresence, motion } from "framer-motion";
 
 const HomeContent: React.FC<{ showAbout: boolean }> = ({ showAbout }) => {
- 
- 
   return (
     <div className={classes.content}>
-      {showAbout && 
-      <h2 className={classes.headabout}>About...</h2>}
+      {showAbout && <h2 className={classes.headabout}>About...</h2>}
       {showAbout && (
-        <motion.div
-          className={classes.abouttext}
-        >
-          {homeContent.main}<br></br>{homeContent.sup}
+        <motion.div className={classes.abouttext}>
+          {homeContent.main}
+          <br></br>
+          {homeContent.sup}
         </motion.div>
       )}
-      {showAbout && 
-      < motion.div
-      >
-      <h3 className={classes.techHead}>Tech...</h3>
-      <section className={classes.techused}>
-        {homeContent.tech.map((tech, index) => {
-          return <div key={index}>{tech}</div>;
-        })}
-      </section>
-      </motion.div> }
+      {showAbout && (
+        <motion.div>
+          <h3 className={classes.techHead}>Tech...</h3>
+          <section className={classes.techused}>
+            {homeContent.tech.map((tech, index) => {
+              return <div key={index}>{tech}</div>;
+            })}
+          </section>
+        </motion.div>
+      )}
     </div>
   );
 };
